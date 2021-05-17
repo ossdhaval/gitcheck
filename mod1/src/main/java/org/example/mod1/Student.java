1d783ef807f48d6e289d70af805b6eadd4e2c632
ossdhaval <ossdhaval@outlook.com>
2021-05-17 14:49:45 +0530

    add test cases

---
+++ mod1/src/main/java/org/example/mod1/Student.java
@@ -0,0 +1,20 @@
+package org.example.mod1;
+
+public class Student{
+
+    String firstName;
+    String lastName;
+
+    public String getFullName()
+    {
+        return firstName+" "+lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
